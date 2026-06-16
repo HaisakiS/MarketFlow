1662d7e428d3b5fcd38d9c90de183b324f32f19c
Mauricio <162849386+HaisakiS@users.noreply.github.com>
2026-06-16 01:30:07 -0500

    Support for multiple variants and sources
    Graphs now differentiate beetween variants and sources of the same product. Show a legend and each Variant/Source has its graph line.

--- analytics.py
+++ analytics.py
@@ -8,16 +8,13 @@ def plot_product_trend(target_product):
         print("Could not connect to the database.")
         return
 
-
     query = """
-        SELECT ph.record_date, ph.recorded_price, ps.source_name
+        SELECT ph.record_date, ph.recorded_price, ph.variant, ps.source_name
         FROM price_history ph
         JOIN price_sources ps ON ph.id_source = ps.id_source
         WHERE ph.product_name = ?
         ORDER BY ph.record_date ASC
     """
-
-    """[TO DO: Differentiate variants, for now it doesn't take them in count]"""
 
     df = pd.read_sql_query(query, conn, params=(target_product,))
     conn.close()
@@ -30,23 +27,26 @@ def plot_product_trend(target_product):
     df['record_date'] = pd.to_datetime(df['record_date'], format='%d/%m/%Y')
     df = df.sort_values(by='record_date')
 
+    #Differentiate variants of teh same product
+    df['legend_label'] = df['source_name'] + " (" + df['variant'] + ")"
+
     #Basic plot using plt library
     plt.figure(figsize=(10, 6))
 
-    for source in df['source_name'].unique():
-        subset = df[df['source_name'] == source]
+    for label in df['legend_label'].unique():
+        subset = df[df['legend_label'] == label]
         plt.plot(
             subset['record_date'], 
             subset['recorded_price'], 
             marker='o', 
             linewidth=2, 
-            label=source
+            label=label
         )
 
     plt.title(f"📈 Price Trend: {target_product}", fontsize=14, fontweight='bold')
     plt.xlabel("Date", fontsize=12)
     plt.ylabel("Average Price", fontsize=12)
-    plt.legend(title="Sources", loc="best")
+    plt.legend(title="Sources & Variants", loc="best")
     plt.grid(True, linestyle='--', alpha=0.6)
     
     plt.xticks(rotation=45) 
