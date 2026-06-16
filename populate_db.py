f95229acdc8c5f9d25c1dfb525294e1bce7b2347
Mauricio <162849386+HaisakiS@users.noreply.github.com>
2026-06-16 01:27:58 -0500

    Populated data now comes from product_list.csv
    Allows to insert larger data list by using .csv file instead of manually adding them on the script.

--- populate_db.py
+++ populate_db.py
@@ -1,5 +1,8 @@
 import sqlite3
 from database import connect_db, initialize_sys
+import csv
+import os
+import sys
 
 def populate_real_data():
     initialize_sys() #Create Tables if don't exist already
@@ -8,7 +11,12 @@ def populate_real_data():
     if conn is None:
         print("Could not connect to the database.")
         return
-        
+    
+    csv_filename = "product_list.csv"
+    if not os.path.exists(csv_filename):
+        print(f"Error: The file '{csv_filename}' was not found in the current folder.")
+        sys.exit(1)  
+    
     try:
         cursor = conn.cursor()
         
@@ -31,52 +39,47 @@ def populate_real_data():
         cursor.executemany("""INSERT INTO price_sources (source_name, type) 
                               VALUES (?, ?);""", source_list)
         
-        print("Populating historical data for charts...")
-        price_list = [
-            ('Gamesir Nova Lite 2', 'Blanco', 40.0, '15/01/2026', 1),
-            ('Gamesir Nova Lite 2', 'Blanco', 42.5, '10/02/2026', 1),
-            ('Gamesir Nova Lite 2', 'Blanco', 38.0, '05/03/2026', 1), 
-            ('Gamesir Nova Lite 2', 'Blanco', 45.0, '12/04/2026', 1),
-            ('Gamesir Nova Lite 2', 'Blanco', 48.0, '13/05/2026', 1),
-            
-            ('Gamesir Nova Lite 2', 'Blanco', 55.0, '20/01/2026', 3),
-            ('Gamesir Nova Lite 2', 'Blanco', 52.0, '15/02/2026', 3),
-            ('Gamesir Nova Lite 2', 'Blanco', 49.0, '10/03/2026', 3), 
-            ('Gamesir Nova Lite 2', 'Blanco', 58.0, '18/04/2026', 3),
-            ('Gamesir Nova Lite 2', 'Blanco', 60.0, '15/05/2026', 3),
-
-            ('Flydigi Dunefox', 'Rosa', 60.0, '01/02/2026', 2),
-            ('Flydigi Dunefox', 'Rosa', 65.0, '15/03/2026', 2),
-            ('Flydigi Dunefox', 'Rosa', 63.0, '18/05/2026', 2),
-            
-            ('Flydigi Dunefox', 'Rosa', 80.0, '10/02/2026', 3),
-            ('Flydigi Dunefox', 'Rosa', 78.0, '20/03/2026', 3),
-            ('Flydigi Dunefox', 'Rosa', 75.0, '20/05/2026', 3),
-            
-            ('Gamesir X5 Lite', 'Negro', 40.0, '15/01/2026', 1),
-            ('Gamesir X5 Lite', 'Negro', 42.5, '10/02/2026', 1),
-            ('Gamesir X5 Lite', 'Negro', 38.0, '05/03/2026', 1), 
-            ('Gamesir X5 Lite', 'Negro', 45.0, '12/04/2026', 1),
-            ('Gamesir X5 Lite', 'Negro', 48.0, '13/05/2026', 1),
-            
-            ('Gamesir X5 Lite', 'Verde', 55.0, '20/01/2026', 1),
-            ('Gamesir X5 Lite', 'Verde', 52.0, '15/02/2026', 1),
-            ('Gamesir X5 Lite', 'Verde', 49.0, '10/03/2026', 1), 
-            ('Gamesir X5 Lite', 'Verde', 58.0, '18/04/2026', 1),
-            ('Gamesir X5 Lite', 'Verde', 60.0, '15/05/2026', 1),
-        ]
+        #Obtaining IDs
+        cursor.execute("SELECT source_name, id_source FROM price_sources;")
+        source_mapping = dict(cursor.fetchall())
         
-        """[TO DO: Read data in .csv for larger lists]"""
+        #Populate with data from the .csv file
+        print(f"Reading '{csv_filename}' and populating history...")
+        
+        price_list = []
+        with open(csv_filename, mode='r', encoding='utf-8') as file:
+            csv_reader = csv.DictReader(file) #Read colums title
+            
+            for row in csv_reader:
+                current_source = row['source_name']
+                
+                if current_source in source_mapping:
+                    id_source = source_mapping[current_source]
+                else:
+                    print(f"Error: Source '{current_source}' doesn't exist in database.")
+                    sys.exit(1)
+                
+                # Saving data from each colum of the current row
+                record = (
+                    row['product_name'], 
+                    row['variant'], 
+                    float(row['recorded_price']),
+                    row['record_date'], 
+                    id_source
+                )
+                price_list.append(record) #adding to the list
         
         cursor.executemany("""INSERT INTO price_history 
                               (product_name, variant, recorded_price, record_date, id_source) 
                               VALUES (?, ?, ?, ?, ?);""", price_list)
 
         conn.commit()
-        print("\nDatabase successfully wiped and re-populated.")
+        print("\nSuccess! {len(history_data)} historical records have been added to the database.")
         
     except sqlite3.Error as e:
-        print(f"Database error: {e}")
+            print(f"Database error: {e}")
+    except Exception as e:
+        print(f"An error occurred while processing the CSV: {e}")
     finally:
         conn.close()
 
